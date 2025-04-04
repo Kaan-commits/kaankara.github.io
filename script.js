b4cbca8e24bcd4ea4405534bb29f5e68a6aac019
Kaan Kara <kaan.kara@yahoo.com>
2025-04-04 09:31:09 +0300

    ilk adımlar

---
+++ script.js
@@ -0,0 +1,13 @@
+// Basit bir örnek JavaScript kodu
+
+// Merhaba Dünya! mesajını konsola yazdırır
+console.log("Merhaba Dünya!");
+
+// Bir fonksiyon tanımlayalım
+function topla(a, b) {
+    return a + b;
+}
+
+// Fonksiyonu kullanarak iki sayıyı toplayalım
+let sonuc = topla(5, 7);
+console.log("Toplam:", sonuc);
